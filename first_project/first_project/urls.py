4fb5554fc05500fc7f22d9226fafcc8d9e483926
user <user@users-iMac.local>
2019-07-04 21:13:50 -0400

    costs

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -33,11 +33,11 @@ urlpatterns = [ #FOR EACH VIEW, INPUT CORRESPONDING VIEWS JOB IN VIEWS.PY
     url(r'^first_app/',include('first_app.urls')), #
 
     #path('/energycosts',views.energycosts),
-    
+
     #url(r'^energycosts/,views.index,name='energycosts'),#returns index from urls.py in directory
-    url(r'^/energycosts/,views.index,name='energycosts'),#returns index from urls.py in directory
-    url('help/', views.help,name='help'),
-    url('energycosts/', views.energycosts,name='help'),
+    url(r'^energycosts/',views.energycosts,name='energycosts'),#returns index from urls.py in directory
+    path(r'^energycosts/',views.energycosts,name='energycosts'),#returns index from urls.py in directory
+
     url('contactus/', views.contactus,name='contactus'),
     url('aboutus/', views.aboutus,name='aboutus'),
     url('images/', views.images,name='images'),
