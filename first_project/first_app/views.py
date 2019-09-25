da5ab7e81ccee7ef1acfea962d6ca614293d0ec9
user <user@users-iMac.local>
2019-09-25 18:43:33 -0400

    test

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -28,10 +28,7 @@ def energycosts(request):
     return render(request, 'first_project/first_app/energycosts.html')
 
 def energyderegulation(request):
-    return render(request, 'test/energyderegulation.html')
+    return render(request, 'energyderegulation.html')
 
 def savetoday(request):
     return render(request, 'first_project/first_app/savetoday.html')
-
-def energyderegulation(request):
-    return render(request, 'first_project/first_app/energyderegulation.html')
