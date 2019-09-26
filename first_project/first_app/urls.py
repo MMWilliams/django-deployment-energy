1fe5aed1cd0a1063f176f16926a294224973d08a
user <user@users-iMac.local>
2019-09-26 19:20:19 -0400

    ok

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -4,24 +4,21 @@ from django.conf.urls import include
 from . import views
 
 
-urlpatterns = [
-
+urlpatterns = [ #FOR EACH VIEW, INPUT CORRESPONDING VIEWS JOB IN VIEWS.PY
     url(r'^$',views.index,name='index'),
-    url(r'contactus/', views.contactus,name='contactus'),
-    url(r'aboutus/', views.aboutus,name='aboutus'),
-    url(r'images/', views.images,name='images'),
-    url(r'articles/', views.articles,name='articles'),
-    url(r'savetoday/', views.savetoday,name='savetoday'),
-    url(r'energyderegulation/', views.energyderegulation),
-    url(r'consulting/', views.consulting,name='consulting'),
-    url(r'energycosts/', views.energycosts,name='energycosts')
+    url(r'^/',views.index,name='index'),#returns index from urls.py in directory
+    url('energycosts/', views.energycosts,name='energycosts'),
+    url('contactus/', views.contactus,name='contactus'),
+    url('aboutus/', views.aboutus,name='aboutus'),
+    url('images/', views.images,name='images'),
+    url('articles/', views.articles,name='articles'),
+    url('savetoday/', views.savetoday,name='savetoday'),
+    url('energyderegulation/', views.energyderegulation,name='energyderegulation'),
+    url('consulting/', views.consulting,name='consulting'),
+    path('admin/', admin.site.urls),
+    url(r'first_app/',include('first_app.urls'))
 
 
-    #url(r'^first_app/',include('first_app.urls')),
-    #this needs to be in the directory file
-    #url(r'^$',views.index,name='index'),
-    #index is housed in views.py
-    #url('energycosts/$',views.energycosts,name='energycosts'),#returns index from urls.py in directory
 
 
 
