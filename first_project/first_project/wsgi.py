979b06ad3883eea539a7b4ef34ac62319e9d7a7d
Maureese Williams <Reese@Maureese.local>
2020-02-13 23:45:44 -0500

    added video 2.0

--- first_project/first_project/wsgi.py
+++ first_project/first_project/wsgi.py
@@ -8,18 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-import dotenv #api data
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
-
-#for api
-dotenv.load_dotenv(
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-)
-
-#This code snippet has an additional os.path.dirname() because
-#wsgi.py needs to look two directories back to find the .env file.
-#This snippet is not the same as the one used for manage.py.
 
 application = get_wsgi_application()
